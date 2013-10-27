620b3af9e01899e28f1e07b90d0cabd1a4e38d5f
imGiantChan <530000693@qq.com>
2013-10-27 22:57:19 +0800

    second commit

---
+++ res/js/newtest.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+//图片透明蒙版
+ function openVessel(strId)
+    {
+        document.getElementById(strId).style.display = "block";
+    }
+ function closeVessel(strId)
+    {
+        document.getElementById(strId).style.display = "none";
+    }
+	
+//右侧边栏浮动效果
+function aa(){
+var obj=document.getElementById("two");
+obj.style.visibility="visible";
+}
+function bb(){
+var obj=document.getElementById("two");
+obj.style.visibility="hidden";
+}
